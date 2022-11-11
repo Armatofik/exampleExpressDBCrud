75d0e0ab5d4c227a42459d899e11e9d87e752981
Armatofik <49724873+Armatofik@users.noreply.github.com>
2022-11-11 12:55:19 +0500

    Update @checkRole.js

--- middlewares/@checkRole.js
+++ middlewares/@checkRole.js
@@ -2,6 +2,7 @@ const STATUS_CODE = require('../consts/statusCode');
 
 module.exports = (needRole) => (req, res, next) => {
   const {role} = req.user;
+
   if (role < needRole) {
     return res.sendStatus(STATUS_CODE.FORBIDDEN);
   }
